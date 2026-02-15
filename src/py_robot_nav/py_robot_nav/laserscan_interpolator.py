67c76addb8dd1cde17dab99a77c07d62c5d932bf
Danya Generalov <danya@danya02.ru>
2026-02-15 17:45:57 +0300

    improvements in interpolator

--- src/py_robot_nav/py_robot_nav/laserscan_interpolator.py
+++ src/py_robot_nav/py_robot_nav/laserscan_interpolator.py
@@ -13,16 +13,12 @@ class Interpolator(Node):
         # Declare parameters
         self.declare_parameter('input_topic', '/scan')
         self.declare_parameter('output_topic', '/interpolated_scan')
-        self.declare_parameter('interpolation_factor', 2.0)
+        self.declare_parameter('target_degree_inc', 0.5)
         
         input_topic = self.get_parameter('input_topic').value
         output_topic = self.get_parameter('output_topic').value
-        self.interpolation_factor = self.get_parameter('interpolation_factor').value
-        
-        if self.interpolation_factor <= 0.0:
-            self.get_logger().error('Interpolation factor must be a positive float > 0')
-            raise ValueError('Invalid interpolation factor')
-        
+        self.target_angle_inc = math.radians(self.get_parameter('target_degree_inc').value)
+
         self.subscription = self.create_subscription(
             LaserScan,
             input_topic,
@@ -35,12 +31,18 @@ class Interpolator(Node):
             output_topic,
             10
         )
-    
-    def resample(self, source_list):
-        target_length = round(len(source_list) * self.interpolation_factor)
+
+    def resample(self, source_list, interpolation_factor):
+        source_array = np.array(source_list)
+        target_length = round(len(source_list) * interpolation_factor)
         x_original = np.linspace(0, 1, len(source_list))
+
+        valid = ~np.isnan(source_array)
         
-        interpolator = interp1d(x_original, source_list, kind='linear')
+        if np.sum(valid) < 2:
+            return [np.nan] * target_length  # Not enough valid points
+
+        interpolator = interp1d(x_original[valid], source_array[valid], kind='linear', fill_value='extrapolate')
         x_new = np.linspace(0, 1, target_length)
         resampled = interpolator(x_new)
         
@@ -51,19 +53,24 @@ class Interpolator(Node):
             self.publisher.publish(msg)
             return
 
-        new_ranges = self.resample(msg.ranges)
+        current_inc = msg.angle_increment
+        target_inc = self.target_angle_inc
+        interp_factor = current_inc / target_inc
+
+        new_ranges = self.resample(msg.ranges, interp_factor)
         new_intensities = None
         if msg.intensities:
-            new_intensities = self.resample(msg.intensities)
+            new_intensities = self.resample(msg.intensities, interp_factor)
 
         new_inc = (msg.angle_max - msg.angle_min) / (len(new_ranges) - 1)
+        # print(f'{current_inc=} {target_inc=} {interp_factor=} {new_inc=}')
         
         new_msg = LaserScan()
         new_msg.header = msg.header
         new_msg.angle_min = msg.angle_min
         new_msg.angle_max = msg.angle_max
         new_msg.angle_increment = new_inc
-        new_msg.time_increment = msg.time_increment / self.interpolation_factor
+        new_msg.time_increment = msg.time_increment / interp_factor
         new_msg.scan_time = msg.scan_time
         new_msg.range_min = msg.range_min
         new_msg.range_max = msg.range_max
@@ -72,6 +79,7 @@ class Interpolator(Node):
             new_msg.intensities = new_intensities
         
         # print('received', len(msg.ranges), 'sent', len(new_msg.ranges))
+        # print(new_msg.ranges)
         self.publisher.publish(new_msg)
 
 def main(args=None):
